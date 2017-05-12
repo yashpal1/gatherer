b7d0019d1a93067938c8b997a5b215926818660f
yashpal1 <yashpal.singh@firmwisegroup.com>
2017-05-12 17:17:05 +0530

    I am done

---
+++ Gatherer/BAL/clsLogin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BAL;
+using DAL;
+using System.Configuration;
+using System.Data;
+
+namespace BAL
+{
+    public class clsLogin
+    {
+        DBManager Sqldbmanager = new DBManager(DataProvider.SqlServer, ConfigurationManager.ConnectionStrings["LMSDBConnectionString"].ConnectionString);
+        public object login(clsJsonMember.loginDetails obj)
+        {
+            try
+            {
+                Sqldbmanager.Open();
+                Sqldbmanager.CreateParameters(2);
+                Sqldbmanager.AddParameters(0, "@UserId", obj.UserId);
+                Sqldbmanager.AddParameters(1, "@Password", obj.Password);
+                return Sqldbmanager.ExecuteDataSet(CommandType.StoredProcedure, "usp_login").ToString();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+
+            }
+            return "";
+        }
+
+    }
+}
