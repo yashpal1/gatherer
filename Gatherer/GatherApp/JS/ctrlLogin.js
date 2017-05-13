62f94fe62ba8f8d9ee49bff988acfad1c4406fc8
yashpal1 <yashpal.singh@firmwisegroup.com>
2017-05-13 11:29:04 +0530

    changed

--- Gatherer/GatherApp/JS/ctrlLogin.js
+++ Gatherer/GatherApp/JS/ctrlLogin.js
@@ -1,7 +1,6 @@
 ﻿/// <reference path="AngularApp.js" />
 /// <reference path="E:\FW Projects\Gather_GIT_2\Gatherer\GatherApp\Scripts/angular.min.js" />
 /// <reference path="E:\FW Projects\Gather_GIT_2\Gatherer\GatherApp\Scripts/angular.js" />
-Mod.controller("ctrlLogin", function ($scope) {
+Mod.controller("ctrlLogin", ['$scope', function ($scope) {
     $scope.Title = { Name: 'Testing' };
-    
-});
+}]);
